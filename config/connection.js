037c22e6d610d73c10610886809d940e4d3f5805
AND.EVV <andrew60199@gmail.com>
2022-11-10 14:44:28 +1100

    added necessary code to start the project

--- config/connection.js
+++ config/connection.js
@@ -12,4 +12,15 @@ const sequelize = process.env.JAWSDB_URL
       },
     });
 
+const testConnection = async () => {
+    try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+}
+
+testConnection()
+
 module.exports = sequelize;
